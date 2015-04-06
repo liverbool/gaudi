cc3545f80605902fe286713e516cae346c52f0fe
ツ Liverbool <nukboon@gmail.com>
2015-04-06 20:56:41 +0700

    update php56

--- templates/php-fpm/setup.sh
+++ templates/php-fpm/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Replace php-fpm user and group
-sed -i 's/user = www-data/user = 0/g' /etc/php5/fpm/pool.d/www.conf
-sed -i 's/group = www-data/group = 0/g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/user = www-data/user = 0/g' /etc/php56/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = 0/g' /etc/php56/fpm/pool.d/www.conf
 
 # Enable error logs
-sed -i 's|;php_flag\[display_errors\]\s*=\s*off|php_flag[display_errors] = on|g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's|;php_flag\[display_errors\]\s*=\s*off|php_flag[display_errors] = on|g' /etc/php56/fpm/pool.d/www.conf
 
 # Add envvars to PHP-FPM configuration files
 envs=`printenv`
@@ -14,5 +14,5 @@ for env in $envs
 do
     IFS== read name value <<< "$env"
 
-    echo "env[$name] = $value" >> /etc/php5/fpm/php-fpm.conf
+    echo "env[$name] = $value" >> /etc/php56/fpm/php-fpm.conf
 done
